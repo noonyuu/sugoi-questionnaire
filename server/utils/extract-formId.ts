df3e1181bc367e191a9c6a2a06f633e4762edec2
shimizu <noon3m.op.sh@gmail.com>
2025-02-28 00:01:26 +0900

    [change] extractFormId関数を通常の関数からアロー関数に変更し、戻り値の型を明示化

--- server/utils/extract-formId.ts
+++ server/utils/extract-formId.ts
@@ -1,4 +1,4 @@
-export function extractFormId(url: string) {
+export const extractFormId = (url: string): string | null => {
   const match = url.match(/[?&]id=([^&]+)/);
   return match ? match[1] : null;
 }
