dfebb5d5ee58d51852d2378866247fe0a7980e5f
shimizu <noon3m.op.sh@gmail.com>
2025-02-27 16:42:06 +0900

    [add] フォームおよび質問、選択肢のモデルを追加

---
+++ server/model/form_model.ts
@@ -0,0 +1,26 @@
+// formのモデル
+export interface Form {
+  id?: number; // 新規作成時は null
+  url: string;
+  formId: string;
+  provider: string;
+  createAt?: string;
+}
+
+// 質問のモデル
+export interface Question {
+  id: number;
+  formId: number;
+  questionText: string;
+  questionType: string;
+  position: number;
+  required: number;
+}
+
+// 選択肢のモデル
+export interface Option {
+  id: number;
+  questionId: number;
+  optionText: string;
+  position: number;
+}
